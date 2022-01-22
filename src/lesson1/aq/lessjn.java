40bccd526c3cd3636d48ab7f3b4199ec73c3dc09
IrynaS1972 <irina612@bk.ru>
2022-01-22 12:18:31 +0300

    initial commit

---
+++ src/lesson1/aq/lessjn.java
@@ -0,0 +1,28 @@
+package lesson1.aq;
+
+import java.util.Scanner;
+
+public class lessjn {
+    public static void main(String[] args) {
+
+ /*   for(int i=-3; i<4; i++){
+        System.out.println(i);
+
+        for (int i=3; i>4; i--);
+        System.out.println("Enter number N:");
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int result=0;
+        for (int i=0; i<=n; i++) {
+  //          result=result+1
+            result +=i;
+        }
+        System.out.println(result);
+
+        int formulaResult = n * (n+1)/2;
+   */     System.out.println("Formula : " + formulaResult);
+
+
+
+    }
+}
