ae5c3762a55bb98ea699eb7ea93b268a8ba25869
IrynaS1972 <irina612@bk.ru>
2022-01-22 12:17:09 +0300

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main   {
+
+public static void main(String[] args) {
+
+   /*     int x = 1;
+        int y = 2;
+        int z = 3;
+
+        if ( x > y && x > z) {
+                System.out.println("int x"); }
+        else if ( y > x && y > z) {
+                System.out.println("int y"); }
+        else if ( z > x && z > y){
+                System.out.println("int z"); }
+*/
+        }
+}
+
